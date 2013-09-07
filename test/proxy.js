dbc18a1edd01536bd1ad49624074a910b2aa2758
wlaurance <w.laurance@gmail.com>
2013-09-07 15:19:55 -0400

    401 for missing auth header
    With or without the presence of the X-Cluster-Id header.

--- test/proxy.js
+++ test/proxy.js
@@ -1,10 +1,47 @@
 var should = require('should');
+var request = require('request');
+var async = require('async');
 
 describe('Lookup proxy location', function() {
+  var host = 'http://127.0.0.1', port;
   before(function(done) {
     process.env.RIAK_RIVER_PROXY_PORT = 8098;
-    require(__dirname + '/../index')(function(){ done(); })();
+    require(__dirname + '/../index')(function(rrp_port){
+      done();
+      port = rrp_port;
+    })();
   });
-  it('should not proxy a bad request');
+  it('should be a 401 for missing Authorization header with or without X-Cluster-Id', function(done) {
+    var headers = [ {}, {"X-Cluster-Id": "Cluster12345"} ];
+
+    function iterator(headers, callback) {
+      function handler(e, r, b) {
+        should.strictEqual(e, null);
+        should.strictEqual(r.statusCode, 401);
+        callback();
+      }
+      request({
+        url: host + ':' + port,
+        headers: headers
+      }, handler);
+    }
+
+    async.each(headers, iterator, done);
+  });
+  it('should not proxy a bad request with bad cluster id', function(done) {
+    function handler(e, r, b) {
+      should.strictEqual(e, null);
+      should.strictEqual(r.statusCode, 400);
+      done();
+    }
+    var opts = {
+      url: host + ':' + port,
+      headers: {
+        Authorization: 'BAD AUTH',
+        "X-Cluster-Id": 'cluster12345'
+      }
+    };
+    request(opts, handler);
+  });
   it('should proxy a good request');
 });
