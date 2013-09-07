73f791edc8d880aa6e24e416d8a02e4fbb893b82
wlaurance <w.laurance@gmail.com>
2013-09-07 14:51:44 -0400

    Abstract server into a closure

--- index.js
+++ index.js
@@ -2,41 +2,50 @@ var http = require('http');
 var httpProxy = require('http-proxy');
 var redis = require('redis');
 var ready;
-var proxy = new httpProxy.RoutingProxy();
 
-var redis_opts = {
-  port: process.env.REDIS_PORT || 6379,
-  host: process.env.REDIS_HOST || '127.0.0.1'
-};
+function buildProxy() {
+  var proxy = new httpProxy.RoutingProxy();
 
-var client = redis.createClient(redis_opts.port, redis_opts.host);
-client.on('ready', startServer);
+  var redis_opts = {
+    port: process.env.REDIS_PORT || 6379,
+    host: process.env.REDIS_HOST || '127.0.0.1'
+  };
 
-function handler(req, res){
-  var auth = req.headers.authorization;
-  var cluster_id = req.headers['x-cluster-id'];
-  if (auth && cluster_id){
-    //check auth with the given cluster id
-    proxy.proxyRequest(req, res, {
-      host: 'localhost',
-      port: 10001
+  var client = redis.createClient(redis_opts.port, redis_opts.host);
+  client.on('ready', startServer);
+
+  function handler(req, res){
+    var auth = req.headers.authorization;
+    var cluster_id = req.headers['x-cluster-id'];
+    if (auth && cluster_id){
+      //check auth with the given cluster id
+      proxy.proxyRequest(req, res, {
+        host: 'localhost',
+        port: 10001
+      });
+    } else {
+      res.writeHead(401);
+      res.end();
+    }
+  }
+
+  var port = process.env.RIAK_RIVER_PROXY_PORT || 8098;
+  console.log(port);
+
+  function startServer() {
+    var server = http.createServer(handler).listen(port, function(){
+      if (process.env.NODE_ENV !== 'PRODUCTION') {
+        console.log('Riak River Proxy running on', port);
+        console.log('Riak River Proxy Redis running on', redis_opts);
+      }
+      if (typeof ready === 'function') ready(port, redis_opts);
     });
-  } else {
-    res.writeHead(401);
-    res.end();
   }
 }
 
-var port = process.env.RIAK_RIVER_PROXY_PORT || 8098;
-
-function startServer() {
-  var server = http.createServer(handler).listen(port, function(){
-    if (process.env.NODE_ENV !== 'PRODUCTION') {
-      console.log('Riak River Proxy running on', port);
-      console.log('Riak River Proxy Redis running on', redis_opts);
-    }
-    if (typeof ready === 'function') ready(port, redis_opts);
-  });
+function exported(done) {
+  ready = done;
+  return buildProxy;
 }
 
-module.exports = function(done) { ready = done; };
+module.exports = exported;
