859563058b56e60c1c1f49e3e55c831e83f21235
wlaurance <w.laurance@gmail.com>
2013-09-08 10:18:26 -0400

    WIP admin api skeleton

---
+++ admin/clusters.js
@@ -0,0 +1,24 @@
+
+var cluster = {
+  list: function(req, res) {
+
+  },
+  delete: function(req, res) {
+
+  },
+  update: function(req, res) {
+
+  }
+};
+
+var clusters = {
+  list: function(req, res) {
+
+  },
+  create: function(req, res) {
+
+  },
+  single: cluster
+};
+
+module.exports = clusters;
---
+++ admin/index.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var ready;
+
+function buildServer() {
+  var clusters = require(__dirname + '/clusters');
+
+  app.get('clusters', clusters.list);
+  app.post('clusters', clusters.create);
+  app.get('clusters/:id', clusters.single.list);
+  app.delete('clusters/:id', clusters.single.delete);
+  app.put('clusters/:id', clusters.single.update);
+
+  var port = process.env.RIAK_RIVER_PROXY_ADMIN_PORT || 3000;
+  http.createServer(app).listen(port, function() {
+    if (process.env.NODE_ENV !== 'PRODUCTION') {
+      console.log('Riak River Admin Proxy API running on', port);
+    }
+    if (typeof ready === 'function') ready(port);
+  });
+}
+
+module.exports = function(done) {
+  ready = done;
+  return buildServer;
+};
