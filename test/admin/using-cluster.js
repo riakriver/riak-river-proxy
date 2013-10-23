6b84641a52ebd73dd9fce079cbfc852815cd1157
wlaurance <w.laurance@gmail.com>
2013-10-23 16:04:32 -0400

    Setup for the using a cluster tests.
    In before block
    - start a fake service on a port using portfinder
    - add the user through the admin api
    - add the cluster throught the admin api

--- test/admin/using-cluster.js
+++ test/admin/using-cluster.js
@@ -1,13 +1,57 @@
 var request = require('request');
 var userid = 'blahblahtestuser123456789';
-var port;
+var portfinder = require('portfinder');
+var async = require('async');
+var adminPort;
 
 function usingCluster() {
+  var fakeServer;
+  var cluster;
   describe('adding a cluster through the admin api', function() {
-    var clusterId;
     before(function(done) {
-      console.log(port);
-      done();
+      function startFakeServer(cb) {
+        var http = require('http');
+        portfinder.getPort(function(err, port) {
+          fakeServer = {
+            handle: http.createServer(function(req, res){
+              res.writeHead(200, {server: 'test-server-handler-' + port});
+              res.end();
+            }),
+            port: port
+          };
+          fakeServer.handle.listen(port, cb);
+        });
+      }
+
+      function addTestCluster(cb) {
+        request.post({
+          url: 'http://127.0.0.1:' + adminPort + '/owners',
+          json: true,
+          body: {
+            owner: {
+              id: userid
+            }
+          }
+        }, function(e,r,b) {
+          request.post({
+            url: 'http://127.0.0.1:' + adminPort + '/clusters',
+            json: true,
+            body: {
+              cluster: {
+                owner: userid,
+                nodes: [{
+                  host: '127.0.0.1',
+                  port: fakeServer.port
+                }]
+              }
+            }
+          }, function(e,r,b) {
+            cluster = b.cluster;
+            cb();
+          });
+        });
+      }
+      async.parallel([startFakeServer, addTestCluster], done);
     });
     it('should show up in the cluster list');
     it('should return a 5xx error if the host is down');
@@ -19,6 +63,6 @@ function usingCluster() {
 }
 
 module.exports = function(p) {
-  port = p;
+  adminPort = p;
   return usingCluster;
 };
